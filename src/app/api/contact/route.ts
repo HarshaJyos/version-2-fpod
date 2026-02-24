5edde6b62878fc80149164cb064b6c280b4a771b
HarshaJyos <harshajyosyabhatla@gmail.com>
2026-02-24 20:21:21 +0530

    app script url updated

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -2,7 +2,7 @@ export async function POST(req: Request) {
     const formData = await req.formData();
 
     const response = await fetch(
-        "https://script.google.com/macros/s/AKfycbzSs_2AbxgpjhaZB90utLCojURO1SabKmEFg3aTxchoUHvOaz3kN4O0reOUtQY2oON9KQ/exec",
+        "https://script.google.com/macros/s/AKfycbydldLhXHN_QZCmnQQkYiLXnp9RnkDRaXzCMHCThVBKbZitQkIzN3TYrg_2mIUyIb_t5w/exec",
         {
             method: "POST",
             body: formData,
